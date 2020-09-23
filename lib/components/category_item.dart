8ddaea2ac8481b59cad41bb060cc6267aac6ed05
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-23 02:16:36 -0300

    aula 1 finalizada

---
+++ lib/components/category_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final Category category;
+
+  CategoryItem(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,8 +7,12 @@ class CategoriesScreen extends StatelessWidget {
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200, //Tamanho máximo para minha child
         childAspectRatio: 3 / 2, // Proporção da minha child (definida de 3 x 2)
-
+        crossAxisSpacing: 20, // Espaçamento no eixo cruzado de 20
+        mainAxisSpacing: 20, // Espaçamento no eixo principal de 20
       ),
+      children: [
+        
+      ],
     );
   }
 }
