cdc90f83d0d20801081115144b37237d70ccb531
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-23 02:22:41 -0300

    Category_item criado e sendo chamado pelo categories_screen, onde é chamado pelo main

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200, //Tamanho máximo para minha child
-        childAspectRatio: 3 / 2, // Proporção da minha child (definida de 3 x 2)
-        crossAxisSpacing: 20, // Espaçamento no eixo cruzado de 20
-        mainAxisSpacing: 20, // Espaçamento no eixo principal de 20
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('Vamos Cozinhar?'),
       ),
-      children: [
-        
-      ],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, //Tamanho máximo para minha child
+          childAspectRatio:
+              3 / 2, // Proporção da minha child (definida de 3 x 2)
+          crossAxisSpacing: 20, // Espaçamento no eixo cruzado de 20
+          mainAxisSpacing: 20, // Espaçamento no eixo principal de 20
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
